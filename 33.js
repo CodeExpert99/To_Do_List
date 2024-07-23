f76536f843d99d0efefc7adde78a8d559be58ab2
Umar <umaraethon@gmail.com>
2024-07-24 01:42:08 +0500

    The Changes: Make the Pop-position more dynamic with javascipt responsiveness, and more dynamic position

--- 33.js
+++ 33.js
@@ -7,7 +7,39 @@ let closepop = document.getElementById("close");
 let tasks = document.getElementById("tasks");
 
 add_new_div.onclick = divclick = () => {
-  pop_id.classList.toggle("pop_up_make_visible");
+  if (pop_id.classList.contains("pop_up_make_visible")) {
+    pop_id.classList.toggle("pop_up_make_visible");
+  } else {
+    pop_id.classList.toggle("pop_up_make_visible");
+    console.log("Scroll Y", window.scrollY);
+    console.log("screen width: ", window.innerWidth);
+    console.log("screen height: ", window.innerHeight);
+
+    if (window.innerWidth <= 820) {
+      pop_id.style.width = window.innerWidth * 0.7 + "px";
+      pop_id.style.height = window.innerHeight * 0.7 + "px";
+    } else if (window.innerWidth < 620) {
+      pop_id.style.width = window.innerWidth * 0.95 + "px";
+      pop_id.style.height = window.innerHeight * 0.95 + "px";
+    } else {
+      pop_id.style.width = window.innerWidth * 0.5 + "px";
+      pop_id.style.height = window.innerHeight * 0.8 + "px";
+    }
+    console.log("pop width ", pop_id.width);
+    pop_id.style.top = window.scrollY + 50 + "px";
+
+    let partmrright = pop_id.getBoundingClientRect().right / 2;
+    //let prmrright = (partmrright / window.innerWidth) * 100;
+    //let partmrleft = pop_id.getBoundingClientRect().left;
+    console.log("pop right dis ", partmrright);
+
+    console.log("pop percent dis ", prmrright);
+    //pop_id.style.marginLeft = prmrright - 6 + "%";
+    pop_id.style.marginLeft = partmrright - 80 + "px";
+
+    pop_id.getBoundingClientRect().right = 0 + "px";
+    pop_id.getBoundingClientRect().left = 0 + "px";
+  }
 };
 
 const fun1 = () => {
